080b3b53b6c92d5245abb7946e5296479931a4ed
Maarten de Vries <maarten@de-vri.es>
2023-02-08 21:24:43 +0100

    Add more proxies.

---
+++ src/generated/mod.rs
@@ -0,0 +1,41 @@
+mod automount;
+pub use automount::*;
+
+mod device;
+pub use device::*;
+
+mod job;
+pub use job::*;
+
+mod manager;
+pub use manager::*;
+
+mod mount;
+pub use mount::*;
+
+mod path;
+pub use path::*;
+
+mod scope;
+pub use scope::*;
+
+mod service;
+pub use service::*;
+
+mod slice;
+pub use slice::*;
+
+mod socket;
+pub use socket::*;
+
+mod swap;
+pub use swap::*;
+
+mod target;
+pub use target::*;
+
+mod timer;
+pub use timer::*;
+
+mod unit;
+pub use unit::*;
